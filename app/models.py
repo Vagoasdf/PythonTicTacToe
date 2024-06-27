9925c2919446bfae6e8351c632be978dbfb5efb5
Vagoasdf <bruno.gutierrez96@gmail.com>
2024-06-27 16:55:40 -0400

    Adding Base Game interaction
    Create,
    Get One
    Get All

--- app/models.py
+++ app/models.py
@@ -15,11 +15,31 @@ class Game(db.Model):
     created_at: Mapped[datetime] = mapped_column(default=db.func.current_timestamp())
     status: Mapped[str] = mapped_column(default='pending')
 
+    def toJson(self):  
+       return {
+            'id': self.id,
+            'status': self.status,
+            'date': self.created_at
+        } 
+    
+    ## Todo: should also return the Moves of the Game object
+    def detailedToJson(self):
+       return "" 
 class Move(db.Model):
     __tablename__ = 'moves'
     id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(default=db.func.current_timestamp())
     gameId: Mapped[int] = mapped_column(ForeignKey('games.id'))
     prevMoveId: Mapped[Optional[int]] = mapped_column(ForeignKey('moves.id'))
     nextMoveId: Mapped[Optional[int]] = mapped_column(ForeignKey('moves.id'))
     coordX: Mapped[int]
     coordY: Mapped[int]
+
+    def toJson(self):  
+       return {
+            'id': self.id,
+            'gameId': self.gameId,
+            'x': self.coordX,
+            'y': self.coordY
+        }
+
